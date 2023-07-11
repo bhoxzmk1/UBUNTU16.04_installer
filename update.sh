3eaa11b590bb8db062a1a490849cd458fcb50769
NONE <94441576+bhoxzmk1@users.noreply.github.com>
2023-07-11 10:34:47 +0800

    update.sh

--- update.sh
+++ update.sh
@@ -4,12 +4,7 @@
 #sudo apt-mark hold firefox
 #sudo apt-mark hold thunderbird
 
-sudo sed -i 's|http://ph.|http://|g' /etc/apt/sources.list
-
 chmod -R 777 /virtualbox
-
-sudo dpkg -i //virtualbox/32bit/AdbeRdr9.4.2-1_i386linux_enu.deb
-sudo apt-get install -f
 
 sudo dpkg -i //virtualbox/32bit/VNC-5.3.2-Linux-x86-DEB/VNC-Viewer-5.3.2-Linux-x86.deb
 sudo apt-get install -f
@@ -17,27 +12,22 @@ sudo apt-get install -f
 sudo dpkg i- //virtualbox/32bit/VNC-5.3.2-Linux-x86-DEB/VNC-Server-5.3.2-Linux-x86.deb
 sudo apt-get install -f
 
-sudo dpkg -i //virtualbox/32bit/webmin_1.881_all.deb
+sudo dpkg -i //virtualbox/32bit/webmin_1.890_all.deb
 sudo apt-get install -f
 
-
 #change server to main server
-
-
-
-#sudo sed -i 's|http://ph.|http://|g' /etc/apt/sources.list
-
-
+sudo sed -i 's|http://ph.|http://|g' /etc/apt/sources.list
 
 #update
-
 sudo apt update
 sudo apt upgrade -y
 
-#installing of needs
-
+#install ing of needs
 sudo apt install gdebi -y
 sudo apt install mc -y
+
+#get 32bit folder 
+#chmod -R 777 /virtualbox 
 
 sudo add-apt-repository ppa:ubuntu-wine/ppa -y
 sudo wget https://dl.winehq.org/wine-builds/Release.key -y
@@ -45,7 +35,6 @@ sudo apt update
 sudo apt install wine1.6 -y
 sudo apt --reinstall install msttcorefonts -y
 sudo apt --reinstall install ttf-mscorefonts-installer -y
-
 #configure wine (/Q/data and set to windows98)
 
 sudo apt install preload -y
@@ -57,8 +46,8 @@ sudo apt install convmv -y
 sudo apt install samba -y
 sudo apt install spark messenger -y
 
-sudo apt remove firefox* -y
-sudo add-apt-repository ppa:plasmazilla/releases -y
+sudo apt remove firefox* 
+sudo add-apt-repository ppa:plasmazilla/releases 
 sudo apt update
 sudo apt install firefox-esr -y
 sudo apt install chromium-browser -y
@@ -73,25 +62,17 @@ sudo apt install automake autoconf libtool -y
 rm -r //virtualbox/32bit/Spark
 rm -r //virtualbox/32bit/unixODBC-2.2.11
 
-tar -xvzf //virtualbox/32bit/spark_2_8_3.tar.gz
 tar -xvzf //virtualbox/32bit/unixodbc_2.2.11.orig.tar.gz
 
-
-#do manually
+mv /home/smdv-dt/Desktop/unixODBC-2.2.11/ //virtualbox/32bit
 
 
 #cd //virtualbox/32bit/unixODBC-2.2.11
 
-#./configure --enable-gui=no
+#./configure --enable-gui=no -y
 
-#make
+#make -y
 
-#make install
+#make install -y
 
 #sudo apt install unixodbc-bin -y
-
-
-
-
-
-
